3c602849d06546ed15251aa46efb4b1203b19619
ThuyLe <thuylv.work@gmail.com>
2025-07-09 16:27:36 +0700

    chap6: add E2B Sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,21 +1,34 @@
+import { Sandbox } from '@e2b/code-interpreter';
 import { Agent, openai, createAgent } from '@inngest/agent-kit';
+import { getSandbox } from '@/lib/sandbox';
 import { inngest } from './client';
 
 export const helloWorld = inngest.createFunction(
   { id: 'hello-world' },
   { event: 'test/hello.world' },
   async ({ event, step }) => {
+    const sandboxId = await step.run('get-sandbox-id', async () => {
+      const sandbox = await Sandbox.create('next-web-ai-builder-3');
+      return sandbox.sandboxId;
+    });
+
     const summaryAgent = createAgent({
-      name: 'Summary Agent',
+      name: 'Code Agent',
       system:
-        'You are a helpful assistant that summarizes text. Summarize the text in 2 words.',
+        'You are an expert Next.js developer. You are given a task to build a Next.js app with shadcn/ui, zustand.',
       model: openai({ model: 'gpt-4o-mini' })
     });
 
     const { output } = await summaryAgent.run(
-      `Summarize this text: ${event.data.text}`
+      `Write the following code: ${event.data.text}`
     );
 
-    return { output };
+    const sandboxUrl = await step.run('get-sandbox-url', async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
