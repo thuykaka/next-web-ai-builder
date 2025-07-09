857f100a68a6077adbc8c578340609ed352f2f0d
ThuyLe <thuylv.work@gmail.com>
2025-07-09 11:44:52 +0700

    init project

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false,
+  logging: {
+    fetches: {
+      fullUrl: true
+    }
+  },
 };
 
 export default nextConfig;
